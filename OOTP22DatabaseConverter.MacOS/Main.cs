ed17bc60f9e6db04d87e560010ef93ac12c95d6f
Steven Leffew <sleffew80@msn.com>
2021-08-18 10:29:59 -0700

    Initial OOTP 22 Database Converter MacOS project.

---
+++ OOTP22DatabaseConverter.MacOS/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace OOTP22DatabaseConverter.MacOS
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
